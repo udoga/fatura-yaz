a55cc8e3b69ed99e0bf06a0289611dea8a67946e
Doga Sezgin <doga.sezgin@gmail.com>
2014-12-03 03:27:49 +0200

    config file unit is millimetre

--- lib/invoice_writer.rb
+++ lib/invoice_writer.rb
@@ -17,26 +17,41 @@ class InvoiceWriter
 
   private
   def write_invoice_data(invoice_data)
-    invoice_data.keys.each do |field_name|
-      if field_name == 'line_items'
-        write_line_items(invoice_data[field_name])
-      else
-        @writer.write invoice_data[field_name], @config.page_item(field_name)
-      end
+    invoice_data.keys.select{|k| k != 'line_items'}.each do |field_name|
+      options = @config.page_item(field_name)
+      @writer.write invoice_data[field_name], convert_millimeters_to_points(options) if options
     end
+    write_line_items invoice_data['line_items']
   end
 
   def write_line_items(line_items)
     line_items.each do |line_item|
       line_item.keys.each do |field|
-        @writer.write line_item[field], @config.page_item("line_items.#{field}")
+        options = @config.page_item("line_items.#{field}")
+        @writer.write line_item[field], convert_millimeters_to_points(options) if options
       end
     end
   end
 
   def write_addenda_contents
     @config.get_addenda_contents.each do |content|
-      @writer.write content, @config.addenda(content)
+      options = @config.addenda(content)
+      @writer.write content, convert_millimeters_to_points(options) if options
     end
+  end
+
+  def convert_millimeters_to_points(options)
+    options.each do |key, value|
+      if [:left, :center, :right].include? key
+        options[key] = value.map {|i| mm_to_pt(i)}
+      elsif [:width, :height].include? key
+        options[key] = mm_to_pt(value)
+      end
+    end
+    options
+  end
+
+  def mm_to_pt(mm)
+    mm * 2.8346
   end
 end
